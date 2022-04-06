9e84ae643d4c95ebb628ffd9a215c76deb298b7d
Jimi <jingmin@toydea.com>
2022-04-06 23:35:23 +0900

    Let Player can move on the draw line

--- CableCarGame/Assets/Script/Paint.cs
+++ CableCarGame/Assets/Script/Paint.cs
@@ -6,10 +6,57 @@ public class Paint : MonoBehaviour
 {
     public GameObject Pancil;
     bool isPreas = false;
+    bool isPlaying = false;
 
+    public GameObject Player;
     List<GameObject> paints = new List<GameObject>();
+    public Vector3[] posList = new Vector3[999];
+    int index = 0;
+    int maxCount = 0;
 
     void Update()
+    {
+        if (isPlaying)
+        {
+            if (Input.GetKey(KeyCode.LeftArrow))
+            {
+                index--;
+            }
+
+            if (Input.GetKey(KeyCode.RightArrow))
+            {
+                index++;
+            }
+
+            index = Mathf.Clamp(index, 0, maxCount - 1);
+            Player.transform.position = posList[index];
+        }
+        else
+        {
+            drawMode();
+        }
+
+
+        if (Input.GetKeyDown(KeyCode.Delete))
+        {
+            for (int i = 0; i < paints.Count; i++)
+            {
+                Destroy(paints[i]);
+            }
+            paints.Clear();
+            isPlaying = false;
+        }
+
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            isPlaying = true;
+
+            maxCount = paints[0].GetComponent<TrailRenderer>().GetPositions(posList);
+            Player.transform.position = posList[index];
+        }
+    }
+
+    void drawMode()
     {
         if (Input.GetMouseButtonDown(0))
         {
@@ -27,15 +74,6 @@ public class Paint : MonoBehaviour
         if (Input.GetMouseButtonUp(0))
         {
             isPreas = false;
-        }
-
-        if (Input.GetKeyDown(KeyCode.Delete))
-        {
-            for (int i = 0; i < paints.Count; i++)
-            {
-                Destroy(paints[i]);
-            }
-            paints.Clear();
         }
     }
 }
